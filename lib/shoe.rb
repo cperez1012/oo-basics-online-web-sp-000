bfbf2198ab7f8e57983727c2f85432c36dba02df
Christian Perez <perezchristian1012@gmail.com>
2019-08-03 16:30:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   attr_reader :brand
- 
+
   def initialize(brand)
     @title = title
   end
- 
+
   def cobble
     puts "Your shoe is as good as new!"
   end
- 
+
 end
